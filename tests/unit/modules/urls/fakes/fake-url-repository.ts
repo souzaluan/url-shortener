077f1e97cc82190d8a0249faec600474d4a3d3a9
Luan <luan.souza69@hotmail.com>
2024-10-19 17:19:24 -0300

    test: shorten url service

---
+++ tests/unit/modules/urls/fakes/fake-url-repository.ts
@@ -0,0 +1,35 @@
+import { randomUUID } from 'node:crypto'
+
+import IUrlRepository from '../../../../../src/modules/urls/repositories/url-repository'
+import { IUrlEntity } from '../../../../../src/modules/urls/domain/url-entity'
+import CreateUrlDTO from '../../../../../src/modules/urls/dtos/create-url-dto'
+
+class FakeUrlRepository implements IUrlRepository {
+  urls: IUrlEntity[] = []
+
+  async create(data: CreateUrlDTO): Promise<IUrlEntity> {
+    const url = {
+      ...data,
+      id: randomUUID(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      deletedAt: null,
+    }
+
+    this.urls.push(url)
+
+    return url
+  }
+
+  async findOneBySlug(slug: string): Promise<IUrlEntity | null> {
+    const url = this.urls.find((url) => url.slug === slug)
+
+    if (!url) {
+      return null
+    }
+
+    return url
+  }
+}
+
+export default FakeUrlRepository
---
+++ tests/unit/modules/urls/services/shorten-url-service.spec.ts
@@ -0,0 +1,32 @@
+import ShortenUrlService from '../../../../../src/modules/urls/services/shorten-url-service'
+import FakeUrlRepository from '../fakes/fake-url-repository'
+
+let fakeUrlRepository: FakeUrlRepository
+let shortenUrlService: ShortenUrlService
+
+describe('ShortenUrlService', () => {
+  beforeEach(() => {
+    fakeUrlRepository = new FakeUrlRepository()
+
+    shortenUrlService = new ShortenUrlService(fakeUrlRepository)
+  })
+
+  it('should be able to shorten a url', async () => {
+    const url = await shortenUrlService.execute({
+      originUrl: 'https://localhost:3000/api/long-url',
+    })
+
+    expect(url).toHaveProperty('id')
+    expect(url).toHaveProperty('shortenedUrl')
+  })
+
+  it('should be able to shorten a url with only 6 characters in the slug', async () => {
+    const url = await shortenUrlService.execute({
+      originUrl: 'https://localhost:3000/api/long-url',
+    })
+
+    const shortened = fakeUrlRepository.urls.find((_url) => _url.id === url.id)
+
+    expect(shortened?.slug).toHaveLength(6)
+  })
+})
