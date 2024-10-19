e547e0296ea4d14efdef615146676714ad727880
Luan <luan.souza69@hotmail.com>
2024-10-19 19:01:42 -0300

    feat: create find origin url by shortened url controller and route

--- src/modules/urls/infra/http/routes/url.routes.ts
+++ src/modules/urls/infra/http/routes/url.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 import ShortenUrlController from '../controllers/shorten-url-controller'
+import FindOriginUrlByShortenedUrlController from '../controllers/find-origin-url-by-shortened-url-controller'
 
 const routes = Router()
 
@@ -8,6 +9,12 @@ routes.post(
   ShortenUrlController.route,
   ShortenUrlController.validator,
   ShortenUrlController.handle,
+)
+
+routes.get(
+  FindOriginUrlByShortenedUrlController.route,
+  FindOriginUrlByShortenedUrlController.validator,
+  FindOriginUrlByShortenedUrlController.handle,
 )
 
 export default routes
