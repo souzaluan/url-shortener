13445c370f286b9c14b2d07968226d58e5631e46
Luan <luan.souza69@hotmail.com>
2024-10-20 00:22:46 -0300

    feat: get urls by user controller and route

--- src/modules/urls/infra/http/routes/url.routes.ts
+++ src/modules/urls/infra/http/routes/url.routes.ts
@@ -2,8 +2,10 @@ import { Router } from 'express'
 
 import ShortenUrlController from '../controllers/shorten-url-controller'
 import FindOriginUrlByShortenedUrlController from '../controllers/find-origin-url-by-shortened-url-controller'
+import GetUrlsByUserController from '../controllers/get-urls-by-user-controller'
 
 import nonRequiredAuthentication from '../../../../../crosscutting/middlewares/non-required-authentication'
+import requiredAuthentication from '../../../../../crosscutting/middlewares/required-authentication'
 
 const routes = Router()
 
@@ -18,6 +20,13 @@ routes.get(
   FindOriginUrlByShortenedUrlController.route,
   FindOriginUrlByShortenedUrlController.validator,
   FindOriginUrlByShortenedUrlController.handle,
+)
+
+routes.get(
+  GetUrlsByUserController.route,
+  requiredAuthentication,
+  GetUrlsByUserController.validator,
+  GetUrlsByUserController.handle,
 )
 
 export default routes
