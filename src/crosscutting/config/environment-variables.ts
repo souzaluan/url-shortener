0f5015e499e444b0bf7524d72c870aa2497b4c61
Luan <luan.souza69@hotmail.com>
2024-10-22 09:15:14 -0300

    refactor: rename environment app port to port

--- src/crosscutting/config/environment-variables.ts
+++ src/crosscutting/config/environment-variables.ts
@@ -1,7 +1,7 @@
 import { cleanEnv, num, str } from 'envalid'
 
 export default cleanEnv(process.env, {
-  APP_PORT: num({ desc: 'API server port', example: '3000' }),
+  PORT: num({ desc: 'API server port', example: '3000' }),
   API_PREFIX_URL: str({ desc: 'API prefix url', example: '/api' }),
   API_URL: str({ desc: 'Full API URL', example: 'http://localhost:3000/api' }),
   DB_HOST: str({ desc: 'Database host', example: 'localhost' }),
